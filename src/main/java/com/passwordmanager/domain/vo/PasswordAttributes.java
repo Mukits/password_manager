49f3449f2b2175290cbb6f45ed92de4cc1c3f407
mukits <mukiturahma99@hotmail.com>
2023-03-31 01:06:47 +0100

    Encryption of the passwords stored

---
+++ src/main/java/com/passwordmanager/domain/vo/PasswordAttributes.java
@@ -0,0 +1,6 @@
+package com.passwordmanager.domain.vo;
+
+public final class PasswordAttributes {
+    public static final int MIN_SIZE = 5;
+    public static final int MAX_SIZE = 100;
+}
--- src/test/java/com/passmanager/password_manager/domain/vo/PasswordAttributes.java
+++
@@ -1,6 +0,0 @@
-package com.passmanager.password_manager.domain.vo;
-
-public class PasswordAttributes {
-    public static final int MIN_SIZE = 5;
-    public static final int MAX_SIZE = 100;
-}
